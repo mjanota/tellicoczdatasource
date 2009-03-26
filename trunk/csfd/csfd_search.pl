ce521d98d1a38112cf2c886d172a57a2d143bc38
janota.martin <janota.martin@2ea30452-1a47-11de-bf1e-7b5f7763e386>
2009-03-26 21:14:35 +0000

    Initial version

---
+++ trunk/csfd/csfd_search.pl
@@ -0,0 +1,403 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE:  csfd_search.pl
+#
+#        USAGE:  ./csfd_search.pl 'movie title'
+#
+#  DESCRIPTION:  Script for searching movies on czech movie database www.csfd.cz
+#
+#      OPTIONS:  ---
+# REQUIREMENTS:  liblwp-useragent-perl
+#		 libhttp-request-perl
+#		 libxml-writer-perl
+#		 libdigest-md5-file-perl	      		    
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Martin Janota (mjan), <janota.m@cce.cz>
+#      COMPANY:  CCE
+#      VERSION:  0.1
+#      CREATED:  30.11.08 11:56:06 CET
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+
+use LWP::UserAgent;
+use HTTP::Request::Common qw { POST };
+use Data::Dumper;
+use XML::Writer;
+use IO::File;
+
+use Digest::MD5;
+use MIME::Base64 qw(encode_base64);
+
+my $ADDRESS = 'http://www.csfd.cz';
+my @MOVIES;
+my @IMAGES;
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_page_post
+#   PARAMETERS:  url, arrray post 
+#      RETURNS:  page content or undef
+#  DESCRIPTION:  stahne html zadaneho url metodou post
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_page_post  {
+	my $ua = LWP::UserAgent->new;
+	my $req = POST $_[0], $_[1];
+	my $res = $ua->request($req);
+	if ($res->is_success) {
+		return $res->as_string
+	}
+	print "False\n";
+	undef;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_page_get
+#   PARAMETERS:  url
+#      RETURNS:  page content or undef 
+#  DESCRIPTION:  stahne html zadaneho url metodou get
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_page_get {
+	my $ua = LWP::UserAgent->new;
+	$ua->agent("$0/1.0" . $ua->agent);
+	my $req = HTTP::Request->new(GET => $_[0] );
+	$req->header('Accept' => 'text/html');
+	
+	my $res = $ua->request($req);
+#	print Dumper($res);
+	if ($res->is_success) {
+		return $res->as_string;
+	}
+	undef;
+}
+
+#===  FUNCTION  ================================================================
+#         NAME:  download_file
+#   PARAMETERS:  url
+#      RETURNS:  1 | 0
+#  DESCRIPTION:  stahne soubor a vrati 1 / 0
+#     COMMENTS:  none
+#===============================================================================
+
+sub download_file {
+	my $ua = LWP::UserAgent->new;
+	my $expected_length;
+	my $bytes_received = 0;
+	my $file = shift;
+	my $req = HTTP::Request->new(GET => shift);
+	my $res = $ua->request($req,$file);
+	if ($res->is_success) {
+#		print "Download OK\n";
+		return 1;
+	}
+#	print $res->status_line, "\n";
+	0;
+}
+
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_ref
+#   PARAMETERS:  html
+#      RETURNS:  array of movie links
+#  DESCRIPTION:  vraci pole adress na jednotlive filmy
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_movie_ref {
+	my $title = shift;
+	my $searchaddr = $ADDRESS . '/hledani-filmu-hercu-reziseru-ve-filmove-databazi/';
+	my @words = split /\s+/, $title;
+	my (@arefs, %hrefs, $html);
+	foreach ( @words ) {
+		my %h;
+		$html = get_page_post($searchaddr,[ search => $_ ]); 
+		$html =~ s/\n//g;
+#		print $html ."\n";
+		while ( $html =~ /href=("|')(\/film\/\d+.*?)\1/g ) { 
+			next if exists $h{$2};
+			$hrefs{$2}++;
+			$h{$2} = 1;
+		}
+	}
+#	print Dumper(\%hrefs);
+	foreach ( keys %hrefs ) {
+		push @{ $arefs[$hrefs{$_}] }, $_;
+	}
+	return @{ $arefs[-1] };
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_data
+#   PARAMETERS:  link to movie
+#      RETURNS:  1/0  
+#  DESCRIPTION:  rozebere stranku s danym, filmem a prevede ji do hashe, ktery ulozi do pole @MOVIES
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_movie_data {
+	my $ref = $ADDRESS . shift;
+	my %movie;
+#	print $ref. "\n";
+	my $html = get_page_get($ref);
+	$html =~ s/\n//g;
+#	print $html ."\n";
+	%movie = get_movie_info($html);
+	$movie{title} = get_movie_title($html);
+	@{ $movie{director} }= get_movie_person($html,'reziser');
+	@{ $movie{cast} } = get_movie_person($html,'herec');
+	for ( my $i = 0; $i < scalar(@{ $movie{cast} }); $i++ ) {
+#		print $i."\n";
+		$movie{cast}[$i] = [ $movie{cast}[$i] ];  
+	}
+	$movie{plot} = get_movie_plot($html,$ref);
+	my %img = get_image($html);
+	if ( %img ) {
+		$movie{cover} = delete $img{cover};
+		push @IMAGES, {%img};
+	} 
+	return %movie;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_title
+#   PARAMETERS:  html
+#      RETURNS:  movie title
+#  DESCRIPTION:  titul, v tagu <h1>titul</h1> 
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_movie_title {
+	$_[0] =~ /<h1.*?>\s*(.*?)\s*<\/h1>/i;
+	my $title = $1;
+	$title =~ s/\s*$//;
+	$title;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_person
+#   PARAMETERS:  html
+#      RETURNS:  array of actors
+#  DESCRIPTION:  herci,reziser vybiram podle odkazu <a href="/herec/...>herec</a>
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_movie_person {
+	my $html = shift;
+	my $person = shift;
+	my (@people,$p);
+	while ( $html =~ /<a\s+href="\/$person\/.*?".*?>(.*?)<\/a>/gi ) {
+		$p = $1;
+		$p =~ s/(&nbsp;)/ /g;
+		$p =~ s/\s*$//;
+		push @people, $p;
+	}
+	@people;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_info
+#   PARAMETERS:  html
+#      RETURNS:  hash
+#  DESCRIPTION:  vraci informace o filmu, zanry, delka trvani, .... 
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_movie_info {
+	my $html = shift;
+	my ($national, %h, $time, $genres,$info );
+	$html =~ /<\/h1>\s*<br>\s*<br>\s*(?:<table.*?<\/table>)?\s*<br>\s*<b>(.*?)<\/b>\s*<BR>\s*<BR>\s*<b>\s*Režie:/i;
+	$html = $1;
+#	print "$html\n";
+	$html =~ s/&nbsp;/ /g;
+	($genres,$info) = split /\s*<br>\s*/i, $html;
+	@{ $h{genre} } = split /\s*\/\s*/, $genres if $genres;
+	if ($info) {
+		($national,$h{year},$time) = split /\s*,\s*/, $info;
+		@{ $h{nationality} } = split /\s*\/\s*/, $national if $national;
+		( $h{time} ) = ( $time =~/(\d+)/ ) if $time;
+	}
+	%h;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_movie_plot
+#   PARAMETERS:  html
+#      RETURNS:  movie plot as string
+#  DESCRIPTION:  vraci obsah filmu 
+#     COMMENTS:  none
+#===============================================================================
+
+
+
+sub get_movie_plot {
+	my $html = shift;
+	$html =~ /href="(\?text=\d+)"/;
+	if ($1) {
+		$html = get_page_get(shift() . $1);
+		$html =~ s/\n//g;
+	}
+	$html =~ /<div style='float:left;width:425px;padding-top:10px;font-weight:normal'>(.*?)<\/div>/;
+	return $1 || '';
+}
+
+#===  FUNCTION  ================================================================
+#         NAME:  get_image
+#   PARAMETERS:  $html
+#      RETURNS:  hash img
+#  DESCRIPTION:  download img_file
+#     COMMENTS:  none
+#===============================================================================
+
+sub get_image {
+	my $html = shift;
+	$html =~ /(http:\/\/img\.csfd\.cz\/posters.*?jpg)/;
+#	print $1."\n";
+	return () unless $1;
+	my $src = $1;
+	my %img;
+	my $file = '/tmp/movie_poster.jpg';
+	unless ( download_file($file,$src) ) {
+		return ();  
+	}
+	my $imgname = generate_imgname($file) . ".jpg";
+	$img{cover} = $imgname; 	
+	$img{format} = 'JPG';
+	$img{id} = $imgname;
+	$img{width} = 134;
+	$img{height} = 180;
+#	$img{'link'} = 'true';
+	$img{data} = file_encode64($file); 
+#	print "$src.\n";
+	return %img;
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  generate_imgname
+#   PARAMETERS:  file
+#      RETURNS:  file md5 sum 
+#  DESCRIPTION:  vrati md5 soucet 	 
+#     COMMENTS:  none
+#===============================================================================
+
+sub generate_imgname {
+	my $file = shift;
+	my $ctx = Digest::MD5->new();
+	open (M,$file);
+	$ctx->addfile(*M);
+	return $ctx->hexdigest;
+	close M;
+#	print $ctx->hexdigest ."\n";
+}
+
+#===  FUNCTION  ================================================================
+#         NAME:  file_encode64
+#   PARAMETERS:  file
+#      RETURNS:  encoded file as string
+#  DESCRIPTION:  
+#     COMMENTS:  none
+#===============================================================================
+
+sub file_encode64 {
+	my $file = shift;
+	open (E,$file);
+	my $txt;
+	while(<E>) {
+		$txt .= $_;	
+	}
+	return encode_base64($txt);
+}
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  perl2xml
+#   PARAMETERS:  
+#      RETURNS:  Tellico XML
+#  DESCRIPTION:  prevadi pole @MOVIES do XML v Tellico formatu
+#     COMMENTS:  none
+#===============================================================================
+
+sub perl2xml {
+	my $fh = new IO::File(*STDOUT);
+	my $out = new XML::Writer(OUTPUT => $fh);
+	print '<?xml version="1.0" encoding="UTF-8"?>';
+	print '<!DOCTYPE tellico PUBLIC "-//Robby Stephenson/DTD Tellico V9.0//EN" "http://periapsis.org/tellico/dtd/v9/tellico.dtd">';
+	$out->startTag("tellico",syntaxVersion => 9, xmlns => "http://periapsis.org/tellico/");
+		$out->startTag("collection", title => "Video", type => 3);
+			$out->startTag("fields");
+				$out->emptyTag("field", name => "_default");
+			$out->endTag("fields");
+	for (my $i = 0; $i < scalar(@MOVIES); $i++) {			
+			$out->startTag("entry",id=>$i);
+		foreach my $key ( sort keys %{ $MOVIES[$i] } ) {
+				if ( ref($MOVIES[$i]->{$key}) eq 'ARRAY' ) {
+					$out->startTag($key.'s');
+						foreach (@{ $MOVIES[$i]->{$key} }) {
+							if ( ref($_) eq 'ARRAY' ){
+								$out->startTag($key);
+									foreach my $v ( @$_ ) {
+										$out->dataElement('column',$v);
+									}
+								$out->endTag($key);
+							} else {
+								$out->dataElement($key,$_);
+							}
+						}
+					$out->endTag($key.'s');
+				} else {
+					$out->dataElement($key,$MOVIES[$i]->{$key});
+				}
+		}
+			$out->endTag("entry");
+	}
+	if ( @IMAGES) {
+			$out->startTag("images");
+		foreach my $img (@IMAGES) {
+				my $data = delete $img->{data};
+				$out->dataElement( "image",$data, %$img );
+				#$out->emptyTag("image",%$img);
+		}
+			$out->endTag("images");
+	}
+		$out->endTag("collection");
+	$out->endTag("tellico");
+	$out->end();
+}
+
+
+
+
+#---------------------------------------------------------------------------
+#  			MAIN BODY
+#---------------------------------------------------------------------------
+
+
+my @movies = get_movie_ref($ARGV[0]);
+exit 1 unless @movies;
+
+#print Dumper(\@movies);
+my %m;
+foreach (@movies) {
+#	print $_."\n";
+	%m = get_movie_data($_);
+	push @MOVIES, { %m };
+}
+
+perl2xml();
+#print Dumper(\@MOVIES);
+
---
+++ trunk/kosmas/kosmas_search.pl
@@ -0,0 +1,294 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use Getopt::Std;
+use Data::Dumper;
+use Cz::Cstocs;
+sub win1250_utf8;
+*win1250_utf8 = new Cz::Cstocs '1250', 'utf8';
+sub utf8_ascii;
+*utf8_ascii = new Cz::Cstocs 'utf8', 'ascii';
+
+use XML::Writer;
+use IO::File;
+
+use Digest::MD5;
+use MIME::Base64 qw(encode_base64);
+
+
+my $ADDRESS = 'http://www.kosmas.cz';
+my $TMPADDR = '/tmp/czech_book_search_' . $$;
+my @BOOKS;
+my @IMAGES;
+
+mkdir $TMPADDR;
+#####################################################
+#                 ARGUMENTS                         #
+#####################################################
+
+my %option = ();
+
+getopt("Hailprt", \%option);
+
+if (defined $option{h}) {
+    print <<EOF;
+Usage: ./czech_book_search.pl
+  -h,       help
+  -a <author>   author of a book
+  -i <isbn>
+  -l <ilustrator>
+  -o <original> Original title
+  -p <publisher>
+  -r <translator>
+  -t <title>
+EOF
+    exit 0;
+}
+
+my %optw = (
+    a => 'autor',
+	i => 'isbn',
+    l => 'ilustrace',
+	o => 'original',
+    p => 'vyrobce',
+    r => 'preklad',
+    t => 'titul',     
+);
+
+my $post_data = join "&", ('hled=Hledej...','pgCnt=100', 
+    map  { 
+	    $option{$_} =~ s/(\s+)/+/g;
+	    $optw{$_} . "=" . lc( utf8_ascii($option{$_}) );
+	 } grep( $option{$_}, keys %option ) );
+#print "$post_data\n";
+
+`wget -q -O $TMPADDR/vysledek.html  --post-data='$post_data' $ADDRESS/hledani_vysledek.asp`;
+ 
+my @refs;
+my $v = get_ref(\@refs);
+
+exit 0 if $v == -1;
+
+if ( $v == 1) {
+	foreach (my $i = 0; $i < scalar(@refs); $i++) {
+		`wget -q -O $TMPADDR/book_${i}.html $refs[$i]`;
+		push @BOOKS, html2perl("$TMPADDR/book_${i}.html");
+	}	
+} else {
+	push @BOOKS, html2perl("$TMPADDR/vysledek.html");
+}
+
+#print Dumper(\@BOOKS);
+&perl2xml if scalar @BOOKS > 0;
+
+`rm -r $TMPADDR`;
+
+#####################################################################
+#					Functions										#
+#####################################################################
+
+
+sub get_ref {
+	my $refs = shift;
+    my $txt = get_html("$TMPADDR/vysledek.html");
+	$txt = win1250_utf8($txt);
+	if ( $txt =~ /<!-- POPIS -->/ ) {
+		return 0
+	}
+	if ( $txt =~ /nebyl nalezen žádný titul/) {
+		return -1;
+	} 
+	@$refs =  $txt =~ /<a href="(http:\/\/www\.kosmas\.cz\/knihy\/\S+?)">/g;
+	return 1
+}
+
+sub get_html {
+    my $file = shift;
+    my $txt;
+    open (H, $file) or die "Couldn't open $file: $!\n";
+    while (<H>) {
+		chomp;
+		$txt .= $_;
+   	}
+    close H;
+    return $txt;
+}
+
+sub html2perl {
+	my %h;
+	my $html = get_html(shift);
+	$html =  win1250_utf8($html);
+	$html =~ /\<\!-- POPIS --\>(.*?)\<\!--/;
+	get_data(\%h,$1);
+	$html =~ /\<\!-- HLAVICKA --\>(.*?)\<\!-- HLAVICKA KONEC --\>/;
+	get_titul(\%h,$1);
+	$html =~ /<img src="(http:\/\/www\.kosmas\.cz\/obalky\/\S+?)"/;
+	push @IMAGES, get_image(\%h,$1) if $1;
+	return \%h; 
+#	print Dumper(\%H);
+}
+
+sub get_data {
+	my $h = shift;
+	my $data = shift;
+	$data =~ s/<br>|<\/h2>/\n/ig;
+	$data =~ s/<.*?>|&nbsp;//g;
+	my ($row);
+	while ($data =~ s/(.*)\n// ) {
+		$row = $1;
+		$row =~ s/^\s+//;
+		$row =~ s/\s+/ /g;
+		next if $row =~ /^\s*$/;
+		$row =~ /^(nakladatel|isbn|orig|form|rok)/i;
+		my $fce = 'get_' . lc($1);
+		eval {
+			no strict;
+			&{$fce}($h,$row) if defined &$fce;		
+	
+		};
+		die $@ if ($@);
+	}
+}
+
+sub get_titul {
+	my $h = shift;
+	my $data = shift;
+	$data =~ /<td>(.*?)<hr/;
+	$data = $1;
+	$data =~ /<h1>(.*?)<\/h1>(.*)/i;
+	$$h{title} = $1;
+	if ($2) {
+		@{ $$h{author} } = map {
+			/<a.*?>(.*?)<\/a>/;
+			get_name($1);
+		} split ', ', $2;
+	}
+}
+
+my $I = 0;
+sub get_image {
+	my $h = shift;
+	my $src = shift;
+	my %img;
+	my $cover = $TMPADDR . "/obalka". $I++ .".gif";
+	`wget -q -O $cover $src`;
+	my $imgname = generate_imgname($cover) . ".gif";
+	$$h{cover} = $imgname; 	
+	$img{format} = 'GIF';
+	$img{id} = $imgname;
+	$img{width} = 100;
+	$img{height} = 150;
+#	$img{'link'} = 'true';
+	$img{data} = file_encode64($cover); 
+#	print "$src.\n";
+	return { %img };
+}
+
+sub get_nakladatel {
+	my ($h,$r) = @_;
+	my ($p,$s) = $r =~ /^Nakladatel: (.*?)(?:, edice: (.*);)?$/;
+	@{$$h{publisher}} = split ',', $p;
+	$$h{series} = $s if $s;
+}
+
+sub get_isbn {
+	my ($h,$r) = @_;
+	$r =~ /^ISBN:\s*((?:\d|-)+)/i;
+	$$h{isbn} = $1 if $1;
+}
+
+sub get_orig {
+	my ($h,$r) = @_;
+	my ($p,$s) = $r =~ /^Originál: (.*?)(?:, překlad: (.*))?$/;
+	$$h{'název-originálu'} = $p if $p;
+	@{ $$h{translator} } = map{ get_name($_) } (split ',', $s) if $s;
+#	$$h{translator} =  get_name($s) if $s;
+}
+
+sub get_form {
+	my ($h,$r) = @_;
+	$r =~ /^Formát: (.*?)$/;
+	my ($p, undef, $l, $b) = split ', ', $1;
+	$p =~ /(\d+)\s*stran/;
+	$$h{pages} = $1 if $1; 
+	$$h{language} = $l if $l;
+	if ($b) {
+	    $b =~ /^(.*?)\s+vazba/;
+	    $$h{binding} = $1 if $1;
+	}
+}
+
+sub get_rok {
+	my ($h,$r) = @_;
+	my ($p,$s) = $r =~ /^Rok vydání:\s*(\d+)(?:[^\(]*\((\d+).*?vydání)?/;
+	$$h{pub_year} = $p if $p;
+	$$h{edition} = $s if $s;
+}
+
+
+sub get_name {
+	my @a = split ' ', $_[0];
+	return  pop(@a) . ", " . join " ", @a;
+}
+
+sub generate_imgname {
+	my $file = shift;
+	my $ctx = Digest::MD5->new();
+	open (M,$file);
+	$ctx->addfile(*M);
+	return $ctx->hexdigest;
+	close M;
+#	print $ctx->hexdigest ."\n";
+}
+
+sub file_encode64 {
+	my $file = shift;
+	open (E,$file);
+	my $txt;
+	while(<E>) {
+		$txt .= $_;	
+	}
+	return encode_base64($txt);
+}
+
+sub perl2xml {
+	my $fh = new IO::File(*STDOUT);
+	my $out = new XML::Writer(OUTPUT => $fh);
+	print '<?xml version="1.0" encoding="UTF-8"?>';
+	print '<!DOCTYPE tellico PUBLIC "-//Robby Stephenson/DTD Tellico V9.0//EN" "http://periapsis.org/tellico/dtd/v9/tellico.dtd">';
+	$out->startTag("tellico",syntaxVersion => 9, xmlns => "http://periapsis.org/tellico/");
+		$out->startTag("collection", title => "My Books", type => 2);
+			$out->startTag("fields");
+				$out->emptyTag("field", name => "_default");
+			$out->endTag("fields");
+	for (my $i = 0; $i < scalar(@BOOKS); $i++) {			
+			$out->startTag("entry",id=>$i);
+		foreach my $key ( sort keys %{ $BOOKS[$i] } ) {
+				if ( ref($BOOKS[$i]->{$key}) eq 'ARRAY' ) {
+					$out->startTag($key.'s');
+						foreach (@{ $BOOKS[$i]->{$key} }) {
+							$out->dataElement($key,$_);
+						}
+					$out->endTag($key.'s');
+				} else {
+					$out->dataElement($key,$BOOKS[$i]->{$key});
+				}
+		}
+			$out->endTag("entry");
+	}
+	if ( @IMAGES) {
+			$out->startTag("images");
+		foreach my $img (@IMAGES) {
+				my $data = delete $img->{data};
+				$out->dataElement( "image",$data, %$img );
+				#$out->emptyTag("image",%$img);
+		}
+			$out->endTag("images");
+	}
+		$out->endTag("collection");
+	$out->endTag("tellico");
+	$out->end();
+}
+
+
